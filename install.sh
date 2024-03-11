684358b4a3d72b44dd950fe1771e8f4686d18ad1
Zhangzhi Peng <59241275+pengzhangzhi@users.noreply.github.com>
2024-03-11 18:28:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ pip install -e .
 pip install -e vendor/esm
 
 pip install torch-scatter torch-sparse torch-cluster torch-spline-conv -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
-pip install torch_geometric biotite
+pip install torch_geometric biotite lmdb tmtools
 pip install dgl-cu113 dglgo -f https://data.dgl.ai/wheels/repo.html
